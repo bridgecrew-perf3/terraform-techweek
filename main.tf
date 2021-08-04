a7b32ad549f66116d90ccc960e9021170f466aa6
Trevon Lawrence <trevon.lawrence@cdw.com>
2021-08-04 16:54:16 -0400

    Initial commit2

--- main.tf
+++ main.tf
@@ -66,3 +66,66 @@ resource "azurerm_virtual_machine" "vm" {
     environment = "staging"
   }
 }
+
+resource "azurerm_network_security_group" "nsg1" {
+  name                = "nsg-1"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+}
+
+resource "azurerm_network_security_rule" "nsr1" {
+  name                        = "ssh"
+  priority                    = 100
+  direction                   = "Outbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "22"
+  destination_port_range      = "22"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.nsg1.name
+}
+
+resource "azurerm_network_security_group" "nsg2" {
+  name                = "nsg-2"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+}
+
+resource "azurerm_network_security_rule" "nsr2" {
+  name                        = "RDP"
+  priority                    = 100
+  direction                   = "Outbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "3389"
+  destination_port_range      = "3389"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.nsg2.name
+}
+
+resource "azurerm_network_security_group" "nsg3" {
+  name                = "nsg-3"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+}
+
+resource "azurerm_network_security_rule" "nsr3" {
+  name                        = "http"
+  priority                    = 100
+  direction                   = "Outbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "80"
+  destination_port_range      = "80"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.nsg3.name
+}
